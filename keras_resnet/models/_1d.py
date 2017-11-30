910bee4fc4d26ebdf9a0f6f6b7aa1493542ff178
rosdyana <rosdyana.kusuma@gmail.com>
2017-11-30 18:45:42 +0800

    rework 1D model

--- keras_resnet/models/_1d.py
+++ keras_resnet/models/_1d.py
@@ -7,124 +7,36 @@ keras_resnet.models._1d
 This module implements popular one-dimensional residual models.
 """
 
-import keras.backend
-import keras.layers
-import keras.models
-import keras.regularizers
-
-import keras_resnet.blocks
-import keras_resnet.layers
-
-
-def ResNet1d(inputs, blocks, block, include_top=True, classes=1000, freeze_bn=True, numerical_names=None, *args, **kwargs):
-    """
-    Constructs a `keras.models.Model` object using the given block count.
-
-    :param inputs: input tensor (e.g. an instance of `keras.layers.Input`)
-
-    :param blocks: the network’s residual architecture
-
-    :param block: a residual block (e.g. an instance of `keras_resnet.blocks.basic_2d`)
-
-    :param include_top: if true, includes classification layers
-
-    :param classes: number of classes to classify (include_top must be true)
-
-    :param freeze_bn: if true, freezes BatchNormalization layers (ie. no updates are done in these layers)
-
-    :param numerical_names: list of bool, same size as blocks, used to indicate whether names of layers should include numbers or letters
-
-    :return model: ResNet model with encoding output (if `include_top=False`) or classification output (if `include_top=True`)
-
-    Usage:
-
-        >>> import keras_resnet.blocks
-        >>> import keras_resnet.models
-
-        >>> shape, classes = (1, 5), 1
-
-        >>> x = keras.layers.Input(shape)
-
-        >>> blocks = [2, 2, 2, 2]
-
-        >>> block = keras_resnet.blocks.basic_2d
-
-        >>> model = keras_resnet.models.ResNet(x, classes, blocks, block, classes=classes)
-
-        >>> model.compile("adam", "categorical_crossentropy", ["accuracy"])
-    """
-    # if keras.bacResNet18_1dkend.image_data_format() == "channels_last":
-    #     axis = 3
-    # else:
-    #     axis = 1
-
-    axis = 1
-
-    if numerical_names is None:
-        numerical_names = [True] * len(blocks)
-
-    x = keras.layers.ZeroPadding1D(padding=1, name="padding_conv1")(inputs)
-    x = keras.layers.Conv1D(64, (1), strides=1,
-                            use_bias=False, name="conv1")(x)
-    x = keras_resnet.layers.BatchNormalization(
-        axis=axis, epsilon=1e-5, freeze=freeze_bn, name="bn_conv1")(x)
-    x = keras.layers.Activation("relu", name="conv1_relu")(x)
-    x = keras.layers.MaxPooling1D(
-        (1), strides=1, padding="same", name="pool1")(x)
-
-    features = 64
-
-    outputs = []
-
-    for stage_id, iterations in enumerate(blocks):
-
-        for block_id in range(iterations):
-            x = block(features, stage_id, block_id, numerical_name=(
-                block_id > 0 and numerical_names[stage_id]), freeze_bn=freeze_bn)(x)
-
-        features *= 2
-
-        outputs.append(x)
-
-    if include_top:
-        assert classes > 0
-
-        x = keras.layers.GlobalAveragePooling2D(name="pool5")(x)
-        x = keras.layers.Dense(classes, activation="softmax", name="fc1000")(x)
-
-        return keras.models.Model(inputs=inputs, outputs=x, *args, **kwargs)
-    else:
-        # Else output each stages features
-        return keras.models.Model(inputs=inputs, outputs=outputs, *args, **kwargs)
+from keras import layers as ll
+from keras.layers import Input, Dense, Activation
+from keras.models import Sequential, Model
+from keras.layers.core import Flatten, RepeatVector, Reshape, Dropout, Masking
+from keras.layers.convolutional import Conv1D, ZeroPadding1D
+from keras.layers.pooling import MaxPooling1D, GlobalMaxPooling1D, AveragePooling1D, GlobalAveragePooling1D
+from keras.layers.merge import dot, multiply, concatenate, add
+from keras.layers.normalization import BatchNormalization
 
 
-def ResNet18_1d(inputs, blocks=None, include_top=True, classes=1000, *args, **kwargs):
-    """
-    Constructs a `keras.models.Model` according to the ResNet18 specifications.
 
-    :param inputs: input tensor (e.g. an instance of `keras.layers.Input`)
-
-    :param blocks: the network’s residual architecture
-
-    :param include_top: if true, includes classification layers
-
-    :param classes: number of classes to classify (include_top must be true)
-
-    :return model: ResNet model with encoding output (if `include_top=False`) or classification output (if `include_top=True`)
-
-    Usage:
-
-        >>> import keras_resnet.models
-
-        >>> shape, classes = (1, 5), 2
-
-        >>> x = keras.layers.Input(shape)
-
-        >>> model = keras_resnet.models.ResNet18_1d(x, classes=classes)
-
-        >>> model.compile("adam", "categorical_crossentropy", ["accuracy"])
-    """
-    if blocks is None:
-        blocks = [2, 2, 2, 2]
-
-    return ResNet1d(inputs, blocks, block=keras_resnet.blocks.basic_1d, include_top=include_top, classes=classes, *args, **kwargs)
+def ResNet18_1d(input_tensor, final_layer_output=220, append='n'):
+    x = Conv1D(
+        64, 7, strides=2, padding='same', name='conv1' + append)(input_tensor)
+    x = BatchNormalization(name='bn_conv1' + append)(x)
+    x = Activation('relu')(x)
+    x = MaxPooling1D(3, strides=2)(x)
+    x = conv_block(x, 3, [64, 64, 256],
+                   stage=2, block='a' + append, strides=1)
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b' + append)
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c' + append)
+    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a' + append)
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b' + append)
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c' + append)
+    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a' + append)
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b' + append)
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c' + append)
+    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a' + append)
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b' + append)
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c' + append)
+    x = AveragePooling1D(final_layer_output, name='avg_pool' + append)(x)
+    x = Flatten()(x)
+    return x
